beab433a09d7e97f836bcf16b6b96daa63ae8e68
Thomas Lottermann <TomLottermann@googlemail.com>
2014-03-03 22:42:57 +0100

    Made httpauth optional

--- klaus/__init__.py
+++ klaus/__init__.py
@@ -59,7 +59,7 @@ class Klaus(flask.Flask):
             self.add_url_rule(rule, view_func=getattr(views, endpoint))
 
 
-def make_app(repos, site_name, use_smarthttp=False, htdigest_file=None):
+def make_app(repos, site_name, use_smarthttp=False, htdigest_file=None, httpauth=False):
     """
     Returns a WSGI app with all the features (smarthttp, authentication)
     already patched in.
@@ -83,36 +83,37 @@ def make_app(repos, site_name, use_smarthttp=False, htdigest_file=None):
             fallback_app=app.wsgi_app,
         )
 
-        # `receive-pack` is requested by the "client" on a push
-        # (the "server" is asked to *receive* packs), i.e. we need to secure
-        # it using authentication or deny access completely to make the repo
-        # read-only.
-        #
-        # Git first sends requests to /<repo-name>/info/refs?service=git-receive-pack.
-        # If this request is responded to using HTTP 401 Unauthorized, the user
-        # is prompted for username and password. If we keep responding 401, Git
-        # interprets this as an authentication failure.  (We can't respond 403
-        # because this results in horrible, unhelpful Git error messages.)
-        #
-        # Git will never call /<repo-name>/git-receive-pack if authentication
-        # failed for /info/refs, but since it's used to upload stuff to the server
-        # we must secure it anyway for security reasons.
-        PATTERN = r'^/[^/]+/(info/refs\?service=git-receive-pack|git-receive-pack)$'
-        if htdigest_file:
-            # .htdigest file given. Use it to read the push-er credentials from.
-            app.wsgi_app = httpauth.DigestFileHttpAuthMiddleware(
-                htdigest_file,
-                wsgi_app=utils.SubUri(dulwich_wrapped_app),
-                routes=[PATTERN],
-            )
-        else:
-            # no .htdigest file given. Disable push-ing.  Semantically we should
-            # use HTTP 403 here but since that results in freaky error messages
-            # (see above) we keep asking for authentication (401) instead.
-            # Git will print a nice error message after a few tries.
-            app.wsgi_app = httpauth.AlwaysFailingAuthMiddleware(
-                wsgi_app=utils.SubUri(dulwich_wrapped_app),
-                routes=[PATTERN],
-            )
+        if httpauth:
+            # `receive-pack` is requested by the "client" on a push
+            # (the "server" is asked to *receive* packs), i.e. we need to secure
+            # it using authentication or deny access completely to make the repo
+            # read-only.
+            #
+            # Git first sends requests to /<repo-name>/info/refs?service=git-receive-pack.
+            # If this request is responded to using HTTP 401 Unauthorized, the user
+            # is prompted for username and password. If we keep responding 401, Git
+            # interprets this as an authentication failure.  (We can't respond 403
+            # because this results in horrible, unhelpful Git error messages.)
+            #
+            # Git will never call /<repo-name>/git-receive-pack if authentication
+            # failed for /info/refs, but since it's used to upload stuff to the server
+            # we must secure it anyway for security reasons.
+            PATTERN = r'^/[^/]+/(info/refs\?service=git-receive-pack|git-receive-pack)$'
+            if htdigest_file:
+                # .htdigest file given. Use it to read the push-er credentials from.
+                app.wsgi_app = httpauth.DigestFileHttpAuthMiddleware(
+                    htdigest_file,
+                    wsgi_app=utils.SubUri(dulwich_wrapped_app),
+                    routes=[PATTERN],
+                )
+            else:
+                # no .htdigest file given. Disable push-ing.  Semantically we should
+                # use HTTP 403 here but since that results in freaky error messages
+                # (see above) we keep asking for authentication (401) instead.
+                # Git will print a nice error message after a few tries.
+                app.wsgi_app = httpauth.AlwaysFailingAuthMiddleware(
+                    wsgi_app=utils.SubUri(dulwich_wrapped_app),
+                    routes=[PATTERN],
+                )
 
     return app
